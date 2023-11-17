fce73a69450beb3b214df73c7d1a9fe444e2460a
farquaad-dev <kikusrybarova@gmail.com>
2023-11-17 22:06:20 +0100

    addded django with home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}"],
+  content: ["./chatapodjavorinou/**/*.{html,js}"],
   theme: {
     extend: {},
   },
