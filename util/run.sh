0bea00d1efde3b810eb95847e5ac68e42b555e9e
farquaad-dev <kikusrybarova@gmail.com>
2023-11-18 23:25:50 +0100

    fix deploy run script

--- util/run.sh
+++ util/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 npm run css-prod
-python manage.py collectstatic --noinput
-python manage.py migrate
-gunicorn django_project.wsgi --bind=0.0.0.0:80
+poetry run python manage.py collectstatic --noinput
+poetry run python manage.py migrate
+poetry run gunicorn django_project.wsgi --bind=0.0.0.0:80
 
