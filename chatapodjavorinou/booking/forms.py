722fb1c2ed63ce76a8290ff2103b79bb94d2eb3f
farquaad-dev <kikusrybarova@gmail.com>
2023-11-18 20:59:54 +0100

    improve reservation form

--- chatapodjavorinou/booking/forms.py
+++ chatapodjavorinou/booking/forms.py
@@ -1,4 +1,6 @@
 from django.forms import ModelForm
+from django.forms.utils import ErrorList
+
 from booking.models import Reservation
 
 
@@ -6,3 +8,8 @@ class ReservationForm(ModelForm):
     class Meta:
         model = Reservation
         fields = ['name', 'surname', 'email', 'phone', 'people', 'date_start', 'date_end', 'message']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'w-full'
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: ["./chatapodjavorinou/**/*.{html,js}"],
+  content: ["./chatapodjavorinou/**/*.{html,js,py}"],
   theme: {
     extend: {
       fontFamily: {
         'sans': ['Poppins', ...defaultTheme.fontFamily.sans]
       },
       colors: {
-        'brown': '#603913',
+        'brown': {
+          DEFAULT: '#603913',
+          light: '#7f4c19',
+        },
       }
     },
   },
-  plugins: [],
+  plugins: [
+      require('@tailwindcss/forms'),
+  ],
 }
 
