fa843220311b2ae6dc427d8a5940069ed65910d0
farquaad-dev <kikusrybarova@gmail.com>
2023-11-18 23:27:16 +0100

    fix deploy run script

--- util/run.sh
+++ util/run.sh
@@ -3,5 +3,5 @@
 npm run css-prod
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py migrate
-poetry run gunicorn django_project.wsgi --bind=0.0.0.0:80
+poetry run gunicorn chatapodjavorinou.wsgi --bind=0.0.0.0:80
 
