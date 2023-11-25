620ca68af944c9df2b6887a6f916020b4c21987a
farquaad-dev <kikusrybarova@gmail.com>
2023-11-25 23:10:13 +0100

    implement calendar selection

--- chatapodjavorinou/booking/static/booking/calendar.js
+++ chatapodjavorinou/booking/static/booking/calendar.js
@@ -1,6 +1,8 @@
 const target = document.querySelector('#calendar');
 const btn_prev = document.querySelector('#calendar-prev');
 const btn_next = document.querySelector('#calendar-next');
+const form_date_start = document.querySelector('#id_date_start');
+const form_date_end = document.querySelector('#id_date_end');
 
 const dateTimeReviver = (key, value) => {
     if (typeof value === 'string') {
@@ -18,9 +20,128 @@ const months = JSON.parse(document.querySelector('#json-months').textContent);
 let showMonth = new Date().getMonth();
 let showYear = new Date().getFullYear();
 
+const dateOrNull = (val) => {
+    if (!val || val === '') return null;
+    const d = new Date(val);
+    if (isNaN(d)) return null;
+    d.setHours(0, 0, 0, 0);
+    return d;
+}
+
+let dateRange = {
+    _start: dateOrNull(form_date_start.value),
+    _end: dateOrNull(form_date_end.value),
+    _hoverEnd: null,
+
+    get start() {
+        return this._start;
+    },
+    set start(date) {
+        if (date <= new Date()) return;
+
+        this._start = date;
+        form_date_start.value = date ? `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}` : '';
+        updateHighlight();
+    },
+
+    get end() {
+        return this._end;
+    },
+    set end(date) {
+        let col = false;
+        for (const reservation of reservations) {
+            if (this.start < reservation.from && date >= reservation.from) {
+                col = true;
+                break;
+            }
+        }
+
+        if (col) {
+            this._start = date;
+        } else {
+            this._end = date;
+        }
+
+        form_date_end.value = date ? `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}` : '';
+        updateHighlight();
+    },
+
+    get hoverEnd() {
+        return this._hoverEnd;
+    },
+    set hoverEnd(date) {
+        for (const reservation of reservations) {
+            if (this.start < reservation.from && date >= reservation.from) {
+                return;
+            }
+        }
+
+        this._hoverEnd = date;
+        updateHighlight();
+    },
+}
+
+const handleClick = (date) => {
+    if (!dateRange.start) {
+        dateRange.start = date;
+        return;
+    }
+    if (date < dateRange.start && !dateRange.end) {
+        dateRange.start = date;
+        dateRange.end = null;
+        return;
+    }
+    if (!dateRange.end) {
+        dateRange.end = date;
+        return;
+    }
+
+    // reset selection
+    dateRange.start = date;
+    dateRange.end = null;
+}
+
+const handleHover = (date) => {
+    if (!dateRange.start) return;
+    dateRange.hoverEnd = date;
+}
+
+const updateHighlight = () => {
+    if (!dateRange.start) return;
+
+    let end = dateRange.end ? dateRange.end : dateRange._hoverEnd;
+    if (!end || end < dateRange.start) {
+        end = dateRange.start;
+    }
+
+    for (let element of target.querySelectorAll('.calendar-day')) {
+        const d = new Date(Number(element.dataset.date));
+
+        if (dateRange.start <= d && d <= end) {
+            element.classList.add('bg-brown', 'bg-brown-light', 'text-white');
+            element.classList.remove(dateRange.end ? 'bg-brown-light' : 'bg-brown');
+        } else {
+            element.classList.remove('bg-brown-light', 'bg-brown', 'text-white');
+        }
+
+        if (d.valueOf() === dateRange.start.valueOf()) {
+            element.classList.add('rounded-l-full');
+        } else {
+            element.classList.remove('rounded-l-full');
+        }
+
+        if (d.valueOf() === end.valueOf()) {
+            element.classList.add('rounded-r-full');
+        } else {
+            element.classList.remove('rounded-r-full');
+        }
+    }
+}
+
 const Calendar = (year, month) => {
     const cnt = new Date(year, month+1, 0).getDate();
     const start = new Date(year, month, 1).getDay();
+    const today = new Date();
 
     let days = [];
     let resI = 0;
@@ -40,6 +161,10 @@ const Calendar = (year, month) => {
             obj.occupied = true;
         }
 
+        if (cd < today) {
+            obj.occupied = true;
+        }
+
         days.push(obj);
     }
 
@@ -49,9 +174,9 @@ const Calendar = (year, month) => {
             <div class="grid grid-cols-7 text-center text-neutral-600 py-2">
                 ${weekdays.map((v) => `<span>${v}</span>`).join('')}
             </div>
-            <div class="grid grid-cols-7">
+            <div class="grid grid-cols-7 gap-y-1">
                 ${days.map(d => `
-                    <div class="flex items-center justify-center aspect-square rounded-full ${d.occupied ? 'text-neutral-300 cursor-not-allowed' : 'hover:bg-red-600 hover:text-white cursor-pointer'}" ${d.day === 1 ? `style="grid-column-start: ${start}"` : ''}>
+                    <div data-date="${d.date.valueOf()}" class="calendar-day flex items-center justify-center aspect-square ${d.occupied ? 'text-neutral-300 cursor-not-allowed' : 'cursor-pointer'}" ${d.day === 1 ? `style="grid-column-start: ${start}"` : ''}>
                         <span>${d.day}</span>
                     </div>
                 `).join('')}
@@ -84,6 +209,14 @@ const redraw = () => {
     const nextMonth = (showMonth + 1 > 11) ? 0 : showMonth + 1;
     const nextYear = (showMonth + 1 > 11) ? showYear + 1 : showYear;
     target.innerHTML = Calendar(showYear, showMonth) + Calendar(nextYear, nextMonth);
+
+    for (const day of target.querySelectorAll('.calendar-day')) {
+        const d = new Date(Number(day.dataset.date));
+        day.onclick = () => handleClick(d);
+        day.onmouseover = () => handleHover(d);
+    }
+
+    updateHighlight();
 }
 
 redraw();
